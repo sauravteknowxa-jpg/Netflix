21f73f3b5a991237c27bf62c106a8ebd2cf382fb
Saurav <saurav.teknowxa@gmail.com>
2025-10-01 15:50:15 +0530

    modified

--- net/src/component/Netflix.jsx
+++ net/src/component/Netflix.jsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
 import "./style.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 function Netflix() {
     const [activeId, setActiveId] = useState(null);
+
+    const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000
+        };
+
   return (
     <>
         <div 
@@ -35,54 +50,67 @@ function Netflix() {
             </div>         
         </div>
 
-        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-8 md:px-8 lg:px-48  lg:py-12">
+        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-8 md:px-8 lg:px-48 py-8 lg:py-12">
             <h2 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 ">Trending Now</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 items-stretch justify-start gap-6 w-full">
-                <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                    <img src="/image/bads.webp" alt="Movie 1" className="w-full h-64 object-cover"/>
-                </div>
-                <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                    <img src="/image/dhadak.webp" alt="Movie 2" className="w-full h-64 object-cover"/>
-                </div>
-                <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                    <img src="/image/narshima.webp" alt="Movie 3" className="w-full h-64 object-cover"/>
-                </div>
-                <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                    <img src="/image/wed.webp" alt="Movie 4" className="w-full h-64 object-cover"/>
-                </div>
-            </div>
+             <div className="w-full mx-auto  ">
+                <Slider {...settings}>
+                        <div className="px-2">
+                            <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+                                <img src="/image/bads.webp" alt="Movie 1" className="w-full h-64 object-cover"/>
+                            </div>
+                        </div>
+                        
+                        <div className="px-2">
+                            <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+                                <img src="/image/dhadak.webp" alt="Movie 2" className="w-full h-64 object-cover"/>
+                            </div>
+                        </div>
+                        
+                        <div className="px-2">
+                            <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+                                <img src="/image/narshima.webp" alt="Movie 3" className="w-full h-64 object-cover"/>
+                            </div>
+                        </div>
+                        
+                        <div className="px-2">
+                            <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+                                <img src="/image/wed.webp" alt="Movie 4" className="w-full h-64 object-cover"/>
+                            </div>
+                        </div>                 
+                </Slider>
+             </div>
         </div>
 
 
 
-        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-6 md:px-8 lg:px-24   lg:py-12">
+        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-6 md:px-8 lg:px-24 py-8 lg:py-12">
             <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6">More reasons to join </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-4 items-stretch justify-start gap-6 w-full">
-                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-4 sm:p-6 md:p-8 lg:p-10 h-full">
-                    
-                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold">Enjoy on your TV.</h2>
-                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-4 items-center justify-start gap-6 w-full">
+                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-2 sm:px-4 md:px-6 lg:px-10 py-6 h-full">
+                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold text-wrap">Enjoy on your TV.</h2>
+                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal text-wrap">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
                 </div>
-                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-4 sm:p-6 md:p-8 lg:p-10 h-full">
-                    
-                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold">Download and go.</h2>
-                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal">Save your favourites easily and always have something to watch.</p>
+
+                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-2 sm:px-4 md:px-6 lg:px-10 py-6 h-full">                    
+                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold text-wrap">Download and go.</h2>
+                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal text-wrap">Save your favourites easily and always have something to watch.</p>
                 </div>
-                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-4 sm:p-6 md:p-8 lg:p-10 h-full">
-                    
-                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold">Watch everywhere.</h2>
-                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal">Stream unlimited movies and TV shows on your phone, tablet, laptop and TV without paying more.</p>
+
+                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-2 sm:px-4 md:px-6 lg:px-10 py-6 h-full">                   
+                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold text-wrap">Watch everywhere.</h2>
+                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal text-wrap">Stream unlimited movies and TV shows on your phone, tablet, laptop and TV without paying more.</p>
                 </div>
-                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-4 sm:p-6 md:p-8 lg:p-10 h-full">
-                    
-                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold">Create profiles for kids.</h2>
-                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal">Send kids on adventures with their favourite characters in a space made just for them—free with your membership.</p>
-                </div>  
+
+                <div className="bg-[#1D1830] flex flex-col items-start justify-start gap-4 rounded-xl p-2 sm:px-4 md:px-6 lg:px-10 py-6 h-full">                   
+                    <h2 className="text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left font-bold text-wrap">Create profiles for kids.</h2>
+                    <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-2xl text-left font-normal text-wrap">Send kids on adventures with their favourite characters in a space made just for them—free with your membership.</p>
+                </div> 
+
             </div>
         </div>
 
 
-        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-8 lg:px-48   lg:py-12">
+        <div className="w-full bg-black flex flex-col items-start justify-start gap-6 text-center px-4 sm:px-8 lg:px-48 py-4 lg:py-12">
             <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6">Frequently Asked Questions </h1>
             <div className="w-full flex flex-col items-center justify-center gap-4">
                 <div 
@@ -190,10 +218,10 @@ function Netflix() {
         </div>
 
 
-        <div className="bg-black flex flex-col items-center justify-center gap-6 text-center px-4 sm:px-8 md:px-48 lg:px-96  lg:py-12">
+        <div className=" bg-black flex flex-col items-center justify-center gap-6 text-center px-4 sm:px-8 md:px-48 lg:px-96 py-8 lg:py-12">
             <p className="text-white text-lg sm:text-xl md:text-2xl lg:text-3xl font-normal ">Ready to watch? Enter your email to create or restart your membership.</p> 
         </div>
-        <div className="w-full bg-black flex flex-col items-center justify-center text-center  px-4 sm:px-8 md:px-16 ">
+        <div className=" w-full bg-black flex flex-col items-center justify-center text-center gap-2 px-4 sm:px-8 md:px-16 py-4 ">
                     <div className="flex flex-col sm:flex-row items-center justify-center w-full">
                                 <input type="text" placeholder="Email address" className="text-gray-800 bg-slate-400 w-full sm:w-auto px-4 py-3 rounded-sm text-lg sm:text-xl lg:text-2xl  mb-4 sm:mb-0"/>
                                 <button className="text-white bg-red-600 px-6 py-3 rounded-sm text-lg sm:text-xl lg:text-2xl font-semibold ml-0 sm:ml-4 w-full sm:w-auto">Get Started &gt;</button>
@@ -242,6 +270,7 @@ function Netflix() {
                     <p className="text-gray-400 text-sm sm:text-lg md:text-xl lg:text-xl font-normal mt-12">This page is protected by Google reCAPTCHA to ensure you're not a bot. <span className="text-blue-500">Learn more.</span> </p>
                 </div>   
         </footer>
+        
     </> 
     );
 }
